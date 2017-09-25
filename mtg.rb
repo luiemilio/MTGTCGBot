ee1a51d41aa2c45668de7b0f5363e1eeb6350e34
Luis Espinola <lespinola@gmail.com>
2017-09-25 19:53:37 -0400

    Refactored

--- mtg.rb
+++ mtg.rb
@@ -6,9 +6,12 @@ require_relative 'token'
 token = Token::KEY
 
 def get_cards(search_term)
-  cards = MTG::Card.where(name: search_term).all[0..5]
-  card_names = []
-  unique_cards = []
+  cards = MTG::Card.where(name: search_term).all[0..9]
+  get_unique_cards(cards)
+end
+
+def get_unique_cards(cards)
+  card_names, unique_cards = [], []
   cards.each do |card|
     next if card_names.include?(card.name) || card.image_url.nil?
     card_names << card.name
@@ -17,21 +20,25 @@ def get_cards(search_term)
   unique_cards
 end
 
+def create_photo_instance(card)
+  Telegram::Bot::Types::InlineQueryResultPhoto.new(
+    id: card.id,
+    photo_url: card.image_url,
+    thumb_url: card.image_url
+  )
+end
+
 Telegram::Bot::Client.run(token) do |bot|
-  inline_query = Telegram::Bot::Types::InlineQuery
   bot.listen do |message|
     case message
-    when inline_query
-      if !message.query.nil? && message.query.length > 4
+    when Telegram::Bot::Types::InlineQuery
+      if !message.query.nil? && message.query.length > 3
         cards = get_cards(message.query)
-        mapped_cards = cards.map do |card|
-          Telegram::Bot::Types::InlineQueryResultPhoto.new(
-            id: card.id,
-            photo_url: card.image_url,
-            thumb_url: card.image_url
-          )
-        end
-        bot.api.answer_inline_query(inline_query_id: message.id, results: mapped_cards)
+        mapped_cards = cards.map { |card| create_photo_instance(card) }
+        bot.api.answer_inline_query(
+          inline_query_id: message.id,
+          results: mapped_cards
+        )
       end
     end
   end
