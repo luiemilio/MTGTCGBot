865b991cf2f5e0f0ff3caee86a24efc69b94b968
Luis Espinola <lespinola@gmail.com>
2017-09-25 17:32:45 -0400

    Uses Telegram inline queries now

--- mtg.rb
+++ mtg.rb
@@ -6,7 +6,7 @@ require_relative 'token'
 token = Token::KEY
 
 def get_cards(search_term)
-  cards = MTG::Card.where(name: search_term).all
+  cards = MTG::Card.where(name: search_term).all[0..5]
   card_names = []
   unique_cards = []
   cards.each do |card|
@@ -14,20 +14,24 @@ def get_cards(search_term)
     card_names << card.name
     unique_cards << card
   end
-  unique_cards.map{|card| card.image_url}
+  unique_cards
 end
 
 Telegram::Bot::Client.run(token) do |bot|
+  inline_query = Telegram::Bot::Types::InlineQuery
   bot.listen do |message|
-    if !message.text.nil?
-      cmd = message.text.split(" ")[0]
-      case cmd
-      when '/card'
-        search_term = message.text.split(" ")[1..-1].join(" ")
-        cards = get_cards(search_term)
-        cards.each do |card_url|
-          bot.api.send_message(chat_id: message.chat.id, text: card_url)
+    case message
+    when inline_query
+      if !message.query.nil? && message.query.length > 4
+        cards = get_cards(message.query)
+        mapped_cards = cards.map do |card|
+          Telegram::Bot::Types::InlineQueryResultPhoto.new(
+            id: card.id,
+            photo_url: card.image_url,
+            thumb_url: card.image_url
+          )
         end
+        bot.api.answer_inline_query(inline_query_id: message.id, results: mapped_cards)
       end
     end
   end
